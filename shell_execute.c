bbc5be3c92264535414a0ae41b5591344f9bf5cb
Colile1 <colilesibanda@gmail.com>
2023-12-10 20:20:27 +0200

    edited shell_execute.c

--- shell_execute.c
+++ shell_execute.c
@@ -16,26 +16,26 @@ int shell_execute(char *input, char **env)
     /* Parse the input to get the command and arguments*/
     args = parse(input, env);
     if (args == NULL || args[0] == NULL) {
-        // Handling empty commands or parsing failure
+        /* Handling empty commands or parsing failure */
         return 1;
     }
 
-    // Find the path of the command
+    /* Find the path of the command */
     cmd_path = find_path(args[0], env);
     if (cmd_path != NULL) {
-        // Run the command if the path is found
+        /* Run the command if the path is found */
         status = run_command(cmd_path, args, env);
-        free(cmd_path); // Remember to free the allocated path
+        free(cmd_path); /* Remember to free the allocated path */
     } else {
-        // If the command path is not found, try to execute it directly (maybe it's a built-in or alias)
+        /* If the command path is not found, try to execute it directly (maybe it's a built-in or alias) */
         status = execute(args, env);
     }
 
-    // Free the arguments array
+    /* Free the arguments array */
     for (int i = 0; args[i] != NULL; i++) {
         free(args[i]);
     }
     free(args);
 
-    return status;
+    return (status);
 }
